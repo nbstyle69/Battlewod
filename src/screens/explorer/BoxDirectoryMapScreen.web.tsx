784eb00271a6b2c59e7e9edfe5ee821a4b37afdc
nbstyle69 <nabil.selmane@gmail.com>
2026-07-13 18:01:45 +0200

    chore: merge all Devin branches + bump iOS build 30 / Android versionCode 48 (v1.0.42)

---
+++ src/screens/explorer/BoxDirectoryMapScreen.web.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function BoxDirectoryMapScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Map not available on web</Text>
+    </View>
+  );
+}
